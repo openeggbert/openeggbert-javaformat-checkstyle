e1ef288db47c4055e69a6df44b2497ef2e73a6d1
Robert Vokac <robertvokac@nanoboot.org>
2022-11-17 16:23:50 +0100

    Import of files to Git

---
+++ src/main/java/org/nanoboot/nanobootchecks/NanobootChecks.java
@@ -0,0 +1,31 @@
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// nanoboot-checks: Encapsulates Nanoboot Checkstyle XML configuration file.
+// Copyright (C) 2018-2022 the original author or authors.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; 
+// version 2.1 of the License only.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+///////////////////////////////////////////////////////////////////////////////////////////////
+package org.nanoboot.nanobootchecks;
+
+/**
+ * A dummy class
+ * @author robertvokac@nanoboot.org
+ */
+public class NanobootChecks {
+    public static void main(String[] args) {
+        System.out.println("Hello. This is only a dummy class and does nothing.");
+    }
+    
+}
